d3a1e664d4d790c3a15c26e3226b71bae9e3b183
Shimizu Tetsuya <shimizu@info.shonan-it.ac.jp>
2021-04-27 09:38:04 +0900

    Lecture01のサンプルと課題解答をアップ

---
+++ Lecture01/code9leap_172212/main.js
@@ -0,0 +1,30 @@
+enchant();
+
+window.onload = function() {
+    //ゲーム全体の設定
+    var game = new Core(320, 320);
+    game.fps = 15;
+    game.preload("betty.png");
+
+    //ゲームが読み込まれたときの処理
+    game.onload = function() {
+        //プレイヤーの生成と設定
+        var player = new Sprite(48, 48);
+        player.image = game.assets["betty.png"];
+        player.x = 0;
+        player.y = 0;
+        player.frame = 8;
+        player.tick = 0;
+        game.rootScene.addChild(player);
+
+        //プレーヤーに関する1フレームの動作
+        player.addEventListener("enterframe", function(e) {
+            if (game.input.down) { //下キー
+                this.y += 4;
+            }
+        });
+    };
+
+    //ゲームスタート
+    game.start();
+};
